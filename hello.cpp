183f21ee27d51118f6133a0ece3456cad9dd0878
Takashi Kurokawa <zonmin10@gmail.com>
2015-02-05 01:04:20 +0900

    make catan class

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class game {
+public:
+    game ();
+    virtual ~game ();
+
+    enum RESOURCE {BRICK, LUMBER, WOOL, GRAIN, ORE};
+    const int RESOURCE_NUM = 5;
+    enum DEVELOPMENT {KNIGHT, PLENTY, ROAD_BUILD, MONOPOLY, VP};
+    const int DEVELOPMENT_NUM = 5;
+
+private:
+    string name;
+    const int TILE_NUM = 19;
+    _map[TILE_NUM];
+};
+
+class player {
+public:
+    player ();
+    virtual ~player ();
+
+private:
+    string name;
+    int _resource[RESOURCE_NUM];
+    int _development[DEVELOPMENT_NUM];
+};
 
 int main(int argc, char const *argv[]) {
     cout << "hello, light622!" << endl;
