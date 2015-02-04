dedc0170ec62d96dd98c9e4c141636d85b2fc2d3
Takashi Kurokawa <zonmin10@gmail.com>
2015-02-04 19:39:28 +0900

    make stable version

---
+++ stable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class mountain {
+public:
+    mountain (arguments);
+    virtual ~mountain ();
+
+private:
+    const int CAMPUS_SIZE = 100;
+    double _height[CAMPUS_SIZE][CAMPUS_SIZE];
+    /* data */
+};
