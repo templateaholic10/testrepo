4abea1265b51e628a8ee48b7a714852ca7dcdc36
Takashi Kurokawa <zonmin10@gmail.com>
2015-05-26 17:53:38 +0900

    EMアルゴリズムのテンプレートを修正

--- EM_algorithm.cpp
+++ EM_algorithm.cpp
@@ -1,5 +1,122 @@
+#include <fstream>
 #include "EM_algorithm.hpp"
 
 namespace EM {
-    
+
+    template <int dim, int num>
+    Data_series<dim, num>::Data_series(std::istream &datain)
+    : _dim(dim), _num(num)
+    {
+        for (int dataid = 0; dataid < num; dataid++) {
+            for (int i = 0; i < dim; i++) {
+                datain >> _x[dataid][i];
+            }
+        }
+    }
+
+    template <int dim, int num, int mixture_num>
+    EM_estimator<dim, num, mixture_num>::EM_estimator(std::istream &datain, std::istream &initialin)
+    : _data(datain), _mixture_num(mixture_num)
+    {
+        std::string line;
+
+        // 初期化
+        _pi.clear();
+        _mu.clear();
+        _sigma.clear();
+        _likelihood.clear();
+
+        _step = 0;
+
+        // 混合率_pi
+        _pi.push_back(std::array<double, mixture_num>());
+        for (int dist = 0; dist < mixture_num; dist++) {
+            initialin >> _pi[0][dist]; getline(initialin, line);
+        }
+        getline(initialin, line);
+
+        // 平均_mu
+        _mu.push_back(std::array<dvector<dim>, mixture_num>());
+        for (int dist = 0; dist < mixture_num; dist++) {
+            _mu[0][dist] = dvector<dim>();
+            for (int i = 0; i < dim; i++) {
+                initialin >> _mu[0][dist](i);
+            }
+            getline(initialin, line);
+        }
+        getline(initialin, line);
+
+        // 分散_sigma
+        _sigma.push_back(std::array<dmatrix<dim>, mixture_num>());
+        for (int dist = 0; dist < mixture_num; dist++) {
+            _sigma[0][dist] = dmatrix<dim>();
+            for (int i = 0; i < dim; i++) {
+                for (int j = 0; j < dim; j++) {
+                    initialin >> _sigma[0][dist](i, j);
+                }
+            }
+            getline(initialin, line);
+        }
+        getline(initialin, line);
+
+        // 対数尤度の期待値_E_logL
+        
+    }
+
+    template <int dim, int num, int mixture_num>
+    void EM_estimator<dim, num, mixture_num>::step()
+    {
+        // 定義式と同じくn+1←nの漸化式を計算する
+
+        // 中間生成物gamma
+        dvector<dim> gamma[mixture_num];
+        double upper, lower;
+        for (int dist = 0; dist < num; dist++) {
+            upper = _pi[step][dist]
+        }
+
+        // 混合率_pi
+
+        // 平均_mu
+
+        // 分散_sigma
+
+        // 対数尤度の期待値
+
+        _step++;
+
+        return;
+    }
+
+    template <int dim, int num, int mixture_num>
+    void EM_estimator<dim, num, mixture_num>::haltCheck()
+    {
+        return;
+    }
+}
+
+constexpr int DIM = 2;
+constexpr int NUM = 100;
+constexpr int MIXTURE_NUM = 2;
+
+int main(int argc, char const *argv[]) {
+    std::ifstream datain("data.txt");
+    std::ifstream initialin("initial.txt");
+
+    EM::EM_estimator<DIM, NUM, MIXTURE_NUM> em(datain, initialin);
+
+    datain.close();
+    initialin.close();
+
+    do {
+        /* code */
+    } while (!em.haltCheck());
+
+    std::ofstream fout("result.csv");
+
+    em.output(fout);
+
+    fout.close();
+
+    return 0;
 }
--- EM_algorithm.hpp
+++ EM_algorithm.hpp
@@ -2,6 +2,7 @@ char *gets(char *str);
 
 #include <iostream>
 #include <vector>
+#include <array>
 #include <boost/numeric/ublas/matrix.hpp>         // (1) 普通の行列用のヘッダ
 #include <boost/numeric/ublas/triangular.hpp>     // (2) 三角行列用のヘッダ
 #include <boost/numeric/ublas/symmetric.hpp>      // (3) 対称行列用のヘッダ
@@ -9,13 +10,15 @@ char *gets(char *str);
 #include <boost/numeric/ublas/matrix_sparse.hpp>  // (5) 疎行列用のヘッダ
 #include <boost/numeric/ublas/vector.hpp>         // (6) ベクトル用のヘッダ
 #include <boost/numeric/ublas/vector_sparse.hpp>  // (7) 疎ベクトル用のヘッダ
-using dvector = boost::numeric::ublas::vector<double>;
-using dmatrix = boost::numeric::ublas::matrix<double>;
+
+// エイリアステンプレート
+template <int size>
+using dvector = boost::numeric::ublas::bounded_vector<double, size>;
+
+template <int size>
+using dmatrix = boost::numeric::ublas::bounded_matrix<double, size, size>;
 
 namespace EM {
-
-    constexpr int DIM = 2;
-    constexpr int NUM = 100;
 
     template <int dim, int num> struct Data_series;
     template <int dim, int num, int mixture_num> class EM_estimator;
@@ -23,28 +26,37 @@ namespace EM {
     template <int dim, int num>
     struct Data_series
     {
-        void input(std::ostream &os);
+        Data_series() = delete;
+        Data_series(std::istream &datain);
 
         int _dim;
         int _num;
-        dvector _x[num];  // 次元はdim，要素数はnum
+        double _x[num][dim];  // 次元はdim，要素数はnum
     };
 
     template <int dim, int num, int mixture_num>
     class EM_estimator
     {
     public:
+        EM_estimator() = delete;
+        EM_estimator(std::istream &datain, std::istream &initialin);
+        ~EM_estimator() = default;
+
         void step();
-        void haltCheck();
+        bool haltCheck() const;
+
+        void output(std::ostream &os) const;
+
     private:
+        int _mixture_num;
         int _step;
-        int _epsilon;
+        static constexpr double _epsilon = 0.01;
 
         Data_series<dim, num> _data;
-        std::vector<double[mixture_num]> _pi;  // ステップ数，分布番号
-        std::vector<dvector[mixture_num]> _mu;  // ステップ数，分布番号，次元
-        std::vector<dmatrix[mixture_num]> _sigma;  // ステップ数，分布番号，次元
-        std::vector<double> _likelihood;  // 対数尤度の期待値
+        std::vector<std::array<double, mixture_num>> _pi;  // _pi[step][dist]：ステップ数，分布番号
+        std::vector<std::array<dvector<dim>, mixture_num>> _mu;  // _mu[step][dist](i)：ステップ数，分布番号，次元
+        std::vector<std::array<dmatrix<dim>, mixture_num>> _sigma;  // _sigma[step][dist](i, j)：ステップ数，分布番号，次元
+        std::vector<double> _E_logL;  // _E_logL[step]：対数尤度の期待値
     };
 
 }
