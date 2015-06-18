209463a38a3f47c555d06acb6af24ef003a517ab
Takashi Kurokawa <zonmin10@gmail.com>
2015-06-18 13:37:59 +0900

    pinwheel tilingにおけるタイルの大きさを調節可能にした

--- lab/test_simple_svg.hpp
+++ lab/test_simple_svg.hpp
@@ -9,7 +9,6 @@
 
 namespace test_simple_svg {
     using namespace svg;
-    // Tの3頂点を与えると，内部に再帰的にpinwheel_tilingを生成する．
 
     inline double length(const Point &P1, const Point &P2)
     {
@@ -28,8 +27,8 @@ namespace test_simple_svg {
         Pinwheel()  = delete;
         ~Pinwheel() = delete;
     private:
-        static constexpr double    min_tile = 30.;
-        static constexpr double    epsilon  = 10e-6;
+        static constexpr double    epsilon = 10e-6;
+        static double              min_tile;
         static Stroke              stroke;
         static std::array <RGB, 2> face_color;
         static std::array <RGB, 2> tail_color;
@@ -37,41 +36,41 @@ namespace test_simple_svg {
         // （内部的）タイリングメソッド
         static void _tile(Document &doc, Point &P1, Point &P2, Point &P3, bool face)
         {
-            // 角度を求める．
-            double theta = atan2(P2.y - P1.y, P2.x - P1.x);
-            // 色を求める．
-            RGB    rgb;
-            double t         = theta / (2 * M_PI) + 1 / 2;
-            auto   get_color = [ = ](int from, int to) -> int {
-                                   return static_cast <int>((1 - t) * from + t * to);
-                               };
-
-            if (face) {
-                std::transform(face_color[0].begin(), face_color[0].end(), face_color[1].begin(), rgb.begin(), get_color);
-            } else {
-                std::transform(tail_color[0].begin(), tail_color[0].end(), tail_color[1].begin(), rgb.begin(), get_color);
-            }
-            // 三角形を描画する．
-            doc << (Polygon(Color(rgb[0], rgb[1], rgb[2]), stroke) << P1 << P2 << P3);
-            // 短辺を求めて，小さければ再帰しない．
+            // 短辺を求めて，小さければ描画．大きければ再帰．
             double d = length(P1, P2);
             if (d < min_tile) {
-                return;
+                // 角度を求める．
+                double theta = atan2(P2.y - P1.y, P2.x - P1.x);
+                // 色を求める．
+                RGB    rgb;
+                double t         = theta / (2 * M_PI) + 1 / 2;
+                auto   get_color = [ = ](int from, int to) -> int {
+                                       return static_cast <int>((1 - t) * from + t * to);
+                                   };
+
+                if (face) {
+                    std::transform(face_color[0].begin(), face_color[0].end(), face_color[1].begin(), rgb.begin(), get_color);
+                } else {
+                    std::transform(tail_color[0].begin(), tail_color[0].end(), tail_color[1].begin(), rgb.begin(), get_color);
+                }
+                // 三角形を描画する．
+                doc << (Polygon(Color(rgb[0], rgb[1], rgb[2]), stroke) << P1 << P2 << P3);
+            } else {
+                // 再帰．
+                Point Q = inner_divide(P3, P2, 0.2);
+                Point R = inner_divide(P1, P2, 0.5);
+                Point S = inner_divide(P1, Q, 0.5);
+                Point T = inner_divide(P2, Q, 0.5);
+                _tile(doc, Q, P1, P3, !face);
+                _tile(doc, S, R, P1, !face);
+                _tile(doc, T, P2, R, !face);
+                _tile(doc, S, R, Q, face);
+                _tile(doc, T, Q, R, face);
             }
-            // 再帰．
-            Point Q = inner_divide(P3, P2, 0.2);
-            Point R = inner_divide(P1, P2, 0.5);
-            Point S = inner_divide(P1, Q, 0.5);
-            Point T = inner_divide(P2, Q, 0.5);
-            _tile(doc, Q, P1, P3, !face);
-            _tile(doc, S, R, P1, !face);
-            _tile(doc, T, P2, R, !face);
-            _tile(doc, S, R, Q, face);
-            _tile(doc, T, Q, R, face);
         }
 
     public:
-        // タイリングメソッド
+        // タイリングメソッド．pinwheelタイルに相似な三角形の3頂点を与えると，Conwayの方法によりタイリングを行う．
         static void tile(Document &doc, const Point &P1, const Point &P2, const Point &P3)
         {
             struct Point_for_sort
@@ -97,6 +96,14 @@ namespace test_simple_svg {
             }
 
             _tile(doc, points[2].P, points[0].P, points[1].P, true);
+        }
+
+        static void set_min_tile(double d)
+        {
+            if (d <= 0.) {
+                return;
+            }
+            min_tile = d;
         }
 
         static void set_color(int r1, int g1, int b1, int r2, int g2, int b2, bool face)
@@ -127,45 +134,25 @@ namespace test_simple_svg {
         }
     };
 
+    double                        Pinwheel::min_tile   = 30.;
     Stroke                        Pinwheel::stroke     = Stroke(0., Color(0, 0, 0));
     std::array <Pinwheel::RGB, 2> Pinwheel::face_color = { 255, 255, 255, 211, 84, 0 };
     std::array <Pinwheel::RGB, 2> Pinwheel::tail_color = { 255, 255, 255, 142, 68, 173 };
 
     void test_pinwheel()
     {
-        // 色の設定
-        // int r1, g1, b1, r2, g2, b2;
-        // if (auto const rgb = util::HSVtoRGB(0, 0, 0)) {
-        //     std::tie(r1, g1, b1) = *rgb;
-        // } else {
-        //     return;
-        // }
-        // if (auto const rgb = util::HSVtoRGB(0, 0, 0)) {
-        //     std::tie(r2, g2, b2) = *rgb;
-        // } else {
-        //     return;
-        // }
-        // Pinwheel::set_color(r1, g1, b1, r2, g2, b2, true);
-        // if (auto const rgb = util::HSVtoRGB(0, 0, 0)) {
-        //     std::tie(r1, g1, b1) = *rgb;
-        // } else {
-        //     return;
-        // }
-        // if (auto const rgb = util::HSVtoRGB(0, 0, 0)) {
-        //     std::tie(r2, g2, b2) = *rgb;
-        // } else {
-        //     return;
-        // }
-        // Pinwheel::set_color(r1, g1, b1, r2, g2, b2, false);
+        // タイルの大きさの設定
+        Pinwheel::set_min_tile(10.);
 
+        // 色の設定
         Pinwheel::set_color(255, 129, 25, 234, 245, 67, true);
         Pinwheel::set_color(158, 40, 163, 213, 132, 224, false);
 
         int        width = 1200, height = 600;
         Dimensions dimensions(width, height);
-        Document   doc("test.svg", Layout(dimensions, Layout::BottomLeft));
+        Document   doc("pinwheel.svg", Layout(dimensions, Layout::BottomLeft));
 
-        Pinwheel::tile(doc, Point(0, 0), Point(width, 0), Point(0, height));
+        Pinwheel::tile(doc, Point(0, 0), Point(2 * width, 0), Point(0, 2 * height));
         doc.save();
     }
 }
