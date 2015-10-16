62ed7cd85495a14d915e0eff41d87c877a96c05e
Takashi Kurokawa <zonmin10@gmail.com>
2015-10-16 18:06:36 +0900

    フェルマーテストの修正

---
+++ fermat_test.R
@@ -0,0 +1,2 @@
+prime_strict <- read.csv("fermat_work4_.csv", header=FALSE)
+prime_prob <- read.csv("fermat_work3_.csv", header=FALSE)
--- fermat_work.cpp
+++ fermat_work.cpp
@@ -1,31 +1,55 @@
 char* gets(char* str);
 
 #include <fstream>
+#include "lab/util.hpp"
 #include "fermat_test.hpp"
 
 namespace prime_test {
+    constexpr size_t n = 10000;
+
     void accum()
     {
-        constexpr size_t n = 10000;
-        std::ofstream fout("fermat_work2.txt");
+        std::ofstream fout("fermat_work2.csv");
         for (size_t i = 1; i <= n; i++) {
-            fout << fermat_test_detail(i, n) << ((i != n) ? ',' : '\n');
+            fout << fermat_test_detail(i, n) << std::endl;
         }
         fout.close();
     }
 
     void precise_fermat_probability()
     {
-        constexpr size_t n = 10000;
-        std::ofstream fout("fermat_work3.txt");
+        std::ofstream fout("fermat_work3.csv");
         for (size_t i = 1; i <= n; i++) {
-            fout << fermat_test_p(i) << ((i != n) ? ',' : '\n');
+            fout << fermat_test_p(i) << std::endl;
         }
         fout.close();
     }
+
+    void prime_list()
+    {
+        std::ofstream fout("fermat_work4.csv");
+        for (size_t i = 1; i <= n; i++) {
+            fout << primitive(i) << std::endl;
+        }
+        fout.close();
+    }
+
+    void variable_test(int p)
+    {
+        std::cout << util::Repeat("-", 20) << std::endl;
+        std::cout << "p = " << p << std::endl;
+        std::cout << "in fact: " << (primitive(p) ? "prime" : "not prime") << std::endl;
+        std::cout << "fermat_test: " << (fermat_test(p) ? "prime" : "not prime") << std::endl;
+        std::cout << "P(p is prime) with fermat_test" << fermat_test_p(p) << std::endl;
+        std::cout << util::Repeat("-", 20) << std::endl;
+    }
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    prime_test::precise_fermat_probability();
+    if (argc == 1) {
+        prime_test::prime_list();
+    } else {
+        prime_test::variable_test(std::stoi(argv[1]));
+    }
 }
