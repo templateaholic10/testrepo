74f3f61b970e3519d13f6d05c5e1c34709eb165e
Takashi Kurokawa <zonmin10@gmail.com>
2015-11-20 11:36:46 +0900

    eigen_ioをDynamicに拡張

--- lab/eigen_io.hpp
+++ lab/eigen_io.hpp
@@ -38,11 +38,112 @@ std::ostream& operator<<(std::ostream& os, const Eigen::Matrix<Elem, m, n>& M)
     return os;
 }
 
+template <typename Elem, int m>
+std::ostream& operator<<(std::ostream& os, const Eigen::Matrix<Elem, m, Eigen::Dynamic>& M)
+{
+    const int n = M.cols();
+    if (m == 0 || n == 0) {
+        return os;
+    }
+    os << M(0, 0);
+    for (int j = 1; j < n; j++) {
+        os << EIGEN_IO_DELIM << M(0, j);
+    }
+    for (int i = 1; i < m; i++) {
+        os << std::endl << M(i, 0);
+        for (int j = 1; j < n; j++) {
+            os << EIGEN_IO_DELIM << M(i, j);
+        }
+    }
+    return os;
+}
+
+template <typename Elem, int n>
+std::ostream& operator<<(std::ostream& os, const Eigen::Matrix<Elem, Eigen::Dynamic, n>& M)
+{
+    const int m = M.rows();
+    if (m == 0 || n == 0) {
+        return os;
+    }
+    os << M(0, 0);
+    for (int j = 1; j < n; j++) {
+        os << EIGEN_IO_DELIM << M(0, j);
+    }
+    for (int i = 1; i < m; i++) {
+        os << std::endl << M(i, 0);
+        for (int j = 1; j < n; j++) {
+            os << EIGEN_IO_DELIM << M(i, j);
+        }
+    }
+    return os;
+}
+
+template <typename Elem>
+std::ostream& operator<<(std::ostream& os, const Eigen::Matrix<Elem, Eigen::Dynamic, Eigen::Dynamic>& M)
+{
+    const int m = M.rows();
+    const int n = M.cols();
+    if (m == 0 || n == 0) {
+        return os;
+    }
+    os << M(0, 0);
+    for (int j = 1; j < n; j++) {
+        os << EIGEN_IO_DELIM << M(0, j);
+    }
+    for (int i = 1; i < m; i++) {
+        os << std::endl << M(i, 0);
+        for (int j = 1; j < n; j++) {
+            os << EIGEN_IO_DELIM << M(i, j);
+        }
+    }
+    return os;
+}
+
 /*! @brief Eigen::Matrix挿入子
 */
 template <typename Elem, int m, int n>
 std::istream& operator>>(std::istream& is, Eigen::Matrix<Elem, m, n>& M)
 {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            is >> M(i, j);
+            is.ignore(1, EIGEN_IO_DELIM);
+        }
+    }
+    return is;
+}
+
+template <typename Elem, int m>
+std::istream& operator>>(std::istream& is, Eigen::Matrix<Elem, m, Eigen::Dynamic>& M)
+{
+    const int n = M.cols();
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            is >> M(i, j);
+            is.ignore(1, EIGEN_IO_DELIM);
+        }
+    }
+    return is;
+}
+
+template <typename Elem, int n>
+std::istream& operator>>(std::istream& is, Eigen::Matrix<Elem, Eigen::Dynamic, n>& M)
+{
+    const int m = M.rows();
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            is >> M(i, j);
+            is.ignore(1, EIGEN_IO_DELIM);
+        }
+    }
+    return is;
+}
+
+template <typename Elem>
+std::istream& operator>>(std::istream& is, Eigen::Matrix<Elem, Eigen::Dynamic, Eigen::Dynamic>& M)
+{
+    const int m = M.rows();
+    const int n = M.cols();
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             is >> M(i, j);
--- lab/main.cpp
+++ lab/main.cpp
@@ -7,7 +7,10 @@
 int main(int argc, char const *argv[])
 {
     constexpr int p = 5;
-    Eigen::dVector<p> v = Eigen::rand_uniform<double, 5, 1>(0., 1.);
+    Eigen::dVector<Eigen::Dynamic> v = Eigen::rand_uniform<double, 5, 1>(0., 1.);
+    _TYPE(v)
+    _DISPLAY(v.rows())
+    _DISPLAY(v.cols())
     const std::string filename("hoge.txt");
     dump(filename, v);
     load(filename, v);
