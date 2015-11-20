36798e018563a91226864959e366ac12ad569e8a
Takashi Kurokawa <zonmin10@gmail.com>
2015-11-20 12:00:54 +0900

    eigen_ioに高度抽出出力関数を追加

--- lab/eigen_io.hpp
+++ lab/eigen_io.hpp
@@ -153,4 +153,165 @@ std::istream& operator>>(std::istream& is, Eigen::Matrix<Elem, Eigen::Dynamic, E
     return is;
 }
 
+namespace Eigen {
+    /*! @brief Eigen::Matrixの高度抽出関数
+        @param os 出力ストリーム
+        @param M 行列
+        @param stand 行列を立体で表現するか，1列で表現するか
+        @param delim デリミタ
+    */
+    template <typename Elem, int m, int n>
+    std::ostream& out(std::ostream& os, const Eigen::Matrix<Elem, m, n>& M, const bool stand = true, const char delim = EIGEN_IO_DELIM)
+    {
+        if (m == 0 || n == 0) {
+            return os;
+        }
+        os << M(0, 0);
+        for (int j = 1; j < n; j++) {
+            os << delim << M(0, j);
+        }
+        for (int i = 1; i < m; i++) {
+            if (stand) {
+                os << std::endl << M(i, 0);
+            } else {
+                os << delim << M(i, 0);
+            }
+            for (int j = 1; j < n; j++) {
+                os << delim << M(i, j);
+            }
+        }
+        return os;
+    }
+
+    template <typename Elem, int m>
+    std::ostream& out(std::ostream& os, const Eigen::Matrix<Elem, m, Eigen::Dynamic>& M, const bool stand = true, const char delim = EIGEN_IO_DELIM)
+    {
+        const int n = M.cols();
+        if (m == 0 || n == 0) {
+            return os;
+        }
+        os << M(0, 0);
+        for (int j = 1; j < n; j++) {
+            os << delim << M(0, j);
+        }
+        for (int i = 1; i < m; i++) {
+            if (stand) {
+                os << std::endl << M(i, 0);
+            } else {
+                os << delim << M(i, 0);
+            }
+            for (int j = 1; j < n; j++) {
+                os << delim << M(i, j);
+            }
+        }
+        return os;
+    }
+
+    template <typename Elem, int n>
+    std::ostream& out(std::ostream& os, const Eigen::Matrix<Elem, Eigen::Dynamic, n>& M, const bool stand = true, const char delim = EIGEN_IO_DELIM)
+    {
+        const int m = M.rows();
+        if (m == 0 || n == 0) {
+            return os;
+        }
+        os << M(0, 0);
+        for (int j = 1; j < n; j++) {
+            os << delim << M(0, j);
+        }
+        for (int i = 1; i < m; i++) {
+            if (stand) {
+                os << std::endl << M(i, 0);
+            } else {
+                os << delim << M(i, 0);
+            }
+            for (int j = 1; j < n; j++) {
+                os << delim << M(i, j);
+            }
+        }
+        return os;
+    }
+
+    template <typename Elem>
+    std::ostream& out(std::ostream& os, const Eigen::Matrix<Elem, Eigen::Dynamic, Eigen::Dynamic>& M, const bool stand = true, const char delim = EIGEN_IO_DELIM)
+    {
+        const int m = M.rows();
+        const int n = M.cols();
+        if (m == 0 || n == 0) {
+            return os;
+        }
+        os << M(0, 0);
+        for (int j = 1; j < n; j++) {
+            os << delim << M(0, j);
+        }
+        for (int i = 1; i < m; i++) {
+            if (stand) {
+                os << std::endl << M(i, 0);
+            } else {
+                os << delim << M(i, 0);
+            }
+            for (int j = 1; j < n; j++) {
+                os << delim << M(i, j);
+            }
+        }
+        return os;
+    }
+
+    /*! @brief Eigen::Matrixの高度挿入関数
+        @param is 入力ストリーム
+        @param M 行列
+        @param delim デリミタ
+    */
+    template <typename Elem, int m, int n>
+    std::istream& in(std::istream& is, Eigen::Matrix<Elem, m, n>& M, const char delim = EIGEN_IO_DELIM)
+    {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                is >> M(i, j);
+                is.ignore(1, delim);
+            }
+        }
+        return is;
+    }
+
+    template <typename Elem, int m>
+    std::istream& in(std::istream& is, Eigen::Matrix<Elem, m, Eigen::Dynamic>& M, const char delim = EIGEN_IO_DELIM)
+    {
+        const int n = M.cols();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                is >> M(i, j);
+                is.ignore(1, delim);
+            }
+        }
+        return is;
+    }
+
+    template <typename Elem, int n>
+    std::istream& in(std::istream& is, Eigen::Matrix<Elem, Eigen::Dynamic, n>& M, const char delim = EIGEN_IO_DELIM)
+    {
+        const int m = M.rows();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                is >> M(i, j);
+                is.ignore(1, delim);
+            }
+        }
+        return is;
+    }
+
+    template <typename Elem>
+    std::istream& in(std::istream& is, Eigen::Matrix<Elem, Eigen::Dynamic, Eigen::Dynamic>& M, const char delim = EIGEN_IO_DELIM)
+    {
+        const int m = M.rows();
+        const int n = M.cols();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                is >> M(i, j);
+                is.ignore(1, delim);
+            }
+        }
+        return is;
+    }
+}
+
 #endif
